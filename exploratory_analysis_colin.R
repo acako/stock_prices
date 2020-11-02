5fde00bdc7584a1cf65c88f538b3ef436bef698a
Colin Green <greenc_35@hotmail.com>
2020-11-01 19:50:31 -0500

    start of exploration

--- exploratory_analysis_colin.R
+++ exploratory_analysis_colin.R
@@ -67,4 +67,34 @@ data_2018 <- data_2018 %>% mutate(year=2018)
 
 # combine all 5 sets
 complete_data <- rbind(data_2014, data_2015, data_2016, data_2017, data_2018)
-# names do not match, need to fix this
+# names do not match, need to fix this...it is because of xyear.price.var
+
+colnames(data_2014)[224] <- 'PRICE.VARR'
+colnames(data_2015)[224] <- 'PRICE.VARR'
+colnames(data_2016)[224] <- 'PRICE.VARR'
+colnames(data_2017)[224] <- 'PRICE.VARR'
+colnames(data_2018)[224] <- 'PRICE.VARR'
+
+complete_data <- rbind(data_2014, data_2015, data_2016, data_2017, data_2018)
+missing_plot(complete_data)
+#difficult to see pattern
+missing_pattern(complete_data)
+missing_count <- sort((sapply(complete_data, function(x) sum(is.na(x)))), decreasing=TRUE)
+
+
+#attempt mice
+
+set.seed(123)
+
+imputed_data_cart <- mice(complete_data, m=1, method='cart',maxit = 1)
+imputed_data_rf <- mice(complete_data, m=1, method='rf',maxit = 1)
+imputed_data_norm <- mice(complete_data, m=1, method='norm.predict',maxit = 1)
+
+complete_cart <- complete(imputed_data_cart,1)
+complete_rf <- complete(imputed_data_rf, 1)
+
+write.csv(complete_data, 'complete_data.csv')
+
+write.csv(complete_cart, 'cart_data.csv')
+
+write.csv(complete_rf, 'rf_data.csv')
