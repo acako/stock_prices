0667eaa2e2c468057577565d566c9725216d2a38
acako <cakoalbina@gmail.com>
2020-11-05 18:42:04 -0500

    decision tree

--- data_exploration_albina.R
+++ data_exploration_albina.R
@@ -13,6 +13,9 @@ data_imp <- read.csv("cart_imputation.csv")
 summary(data_imp)
 
 #remove data with missing values
+data_clean <-data_imp - c[24, 30, 31, 33])
+model_final_price_1 <- train(final_price ~.,data=complete_d_1,trControl = trainControl("cv",number=10, savePredictions = 'all'),
+                             method='xgbTree', tuneGrid = grid)
 
   
 
