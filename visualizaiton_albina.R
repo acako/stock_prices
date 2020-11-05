c4dc26a3b4c226b28060bc6221921eb590d609e5
acako <cakoalbina@gmail.com>
2020-11-05 17:59:49 -0500

    loading imputated data

--- visualizaiton_albina.R
+++ visualizaiton_albina.R
@@ -7,4 +7,12 @@ library(ggplot2)
 library(reshape2)
 ggplot(melt(data),aes(x=value)) + geom_histogram() + facet_wrap(~variable)
 
+#load imputed dataset
+data_imp <- read.csv("cart_imputation.csv")
+
+summary(data_imp)
+
+#remove data with missing values
+
+  
 
