31b9ec355f9dbb8ab65a9a243e17f8c5f255a914
acako <cakoalbina@gmail.com>
2020-11-05 17:49:55 -0500

    summary

--- visualizaiton_albina.R
+++ visualizaiton_albina.R
@@ -1,7 +1,8 @@
 data <- read.csv("exploration_data.csv")
+#data summary
 summary(data)
 
-#multiple histogram plot
+#multiple histogram plot for missing data
 library(ggplot2)
 library(reshape2)
 ggplot(melt(data),aes(x=value)) + geom_histogram() + facet_wrap(~variable)
