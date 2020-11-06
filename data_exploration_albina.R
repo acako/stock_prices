ab0924fb574119977cd5077e27d86b90e8087e58
acako <cakoalbina@gmail.com>
2020-11-06 15:56:54 -0500

    decision tree

--- data_exploration_albina.R
+++ data_exploration_albina.R
@@ -11,12 +11,29 @@ library(dplyr)
 data_imp <- read.csv("cart_imputation.csv")
 
 summary(data_imp)
-library(cart)
 
 #remove data with missing values
 data_clean <- select(data_imp, -c(Total.non.current.assets, Deferred.revenue, Tax.Liabilities, Total.non.current.liabilities))
+#load libraries
+library(caret)
+library(rpart)
+library(xgboost)
+library(tidyverse)
+#set seed
+summary(data_clean)
+set.seed(100)
+#run decision tree
 dec_tree <- train(Market.Cap ~.,data=data_clean,trControl = trainControl("cv",number=10, savePredictions = 'all'),
-                             method='rPart')
+                             method='rpart')
 
-  
+summary(dec_tree)
+print(dec_tree)
+plot(dec_tree)
+attributes(dec_tree)
 
+#model accuracy
+min(dec_tree$results$RMSE)
+max(dec_tree$results$Rsquared)
+max(dec_tree$resuls$MAE)
+library(rpart.plot)
+rpart.plot(dec_tree, box.palette="RdBu", shadow.col="gray", nn=TRUE)
