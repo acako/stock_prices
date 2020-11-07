1c19fae19133c3e588c3d9aaba0bbed52e1a69a5
acako <cakoalbina@gmail.com>
2020-11-06 20:03:14 -0500

    decision tree model on updated data

--- data_exploration_albina.R
+++ data_exploration_albina.R
@@ -8,12 +8,12 @@ library(reshape2)
 ggplot(melt(data),aes(x=value)) + geom_histogram() + facet_wrap(~variable)
 library(dplyr)
 #load imputed dataset
-data_imp <- read.csv("cart_imputation.csv")
+data_imp <- read.csv("full_set.csv")
 
 summary(data_imp)
 
 #remove data with missing values
-data_clean <- select(data_imp, -c(Total.non.current.assets, Deferred.revenue, Tax.Liabilities, Total.non.current.liabilities))
+data_clean <- select(data_imp, -c(X.1, X))
 #load libraries
 library(caret)
 library(rpart)
@@ -36,4 +36,10 @@ min(dec_tree$results$RMSE)
 max(dec_tree$results$Rsquared)
 max(dec_tree$resuls$MAE)
 library(rpart.plot)
-rpart.plot(dec_tree, box.palette="RdBu", shadow.col="gray", nn=TRUE)
+#rpart.plot(dec_tree, box.palette="RdBu", shadow.col="gray", nn=TRUE)
+#rpart.plot(fit, extra= 106)
+
+saveRDS(dec_tree, 'decisiontree_model_updated_albina.rds')
+tmp <- rownames(object$splits)
+
+summary.rpart(dec_tree)
