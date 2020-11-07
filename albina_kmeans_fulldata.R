b5e6630f903c2ed107bc3a89295cff047250985b
acako <cakoalbina@gmail.com>
2020-11-07 15:57:16 -0500

    k-means on the full data

--- albina_kmeans_fulldata.R
+++ albina_kmeans_fulldata.R
@@ -1,5 +1,5 @@
 #load dataset
-data <- read.csv("important.csv")
+data <- read.csv("full_set.csv")
 #data summary
 summary(data)
 #kmeans(x, centers, iter.max = 10, nstart = 1)
@@ -7,18 +7,22 @@ summary(data)
 library(factoextra)
 library(cluster)
 library(dplyr)
-data_clean <- select(data, -c(X.1, X))
+data_clean <- select(data, -c(X.1, X, Sector))
+summary(data_clean)
+data_clean$Sector <- factor(data_clean$Sector)
 #Silhouette Method for finding the optimal number of clusters
 #install.packages("NbClust")
 library(NbClust)
 fviz_nbclust(data_clean, kmeans, method = "silhouette") + labs(subtitle = "Silhouette method")
+#can scale data 
 #data_scaled <- scale(data_clean)
 #fviz_nbclust(data_scaled, kmeans, method = "silhouette") + labs(subtitle = "Silhouette method")
-#selected top 3: 2, 3 and 4 clusters
+
+#selected top 5: 2, 3 and 4, 5, 6 clusters - did not used scaled data
 
 #2 clusters
 set.seed(123)
-km.res <- kmeans(data_clean, 2, nstart = 25)
+km.res <- kmeans(data_clean, 2, nstart = 50)
 print(km.res)
 km.res$cluster
 head(km.res$cluster, 2)
@@ -39,3 +43,22 @@ print(km.res)
 km.res$cluster
 head(km.res$cluster, 3)
 fviz_cluster(km.res, data_clean, ellipse.type = "norm")
+
+#6 clusters
+set.seed(123)
+km.res <- kmeans(data_clean, 6, nstart = 50)
+print(km.res)
+km.res$cluster
+head(km.res$cluster, 6)
+fviz_cluster(km.res, data_clean, ellipse.type = "norm")
+
+#5 clusters
+set.seed(123)
+km.res <- kmeans(data_clean, 5, nstart = 50)
+print(km.res)
+km.res$cluster
+head(km.res$cluster, 5)
+fviz_cluster(km.res, data_clean, ellipse.type = "norm")
+
+
+
